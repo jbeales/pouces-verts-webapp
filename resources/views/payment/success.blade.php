590a77d8cba54c82e17b4044a3ec374cd3accefc
John Beales <john@johnbeales.com>
2022-05-06 22:52:13 -0400

    Add link to make another payment to the payment complete page.

--- resources/views/payment/success.blade.php
+++ resources/views/payment/success.blade.php
@@ -11,6 +11,7 @@
     <section>
         <p>Votre paiement a été complété. Merci beaucoup!</p>
         <p lang="en-CA">Your payment is complete! Thank you very much!</p>
+        <p><a href="{{route('payment.start')}}">Effectuer un autre paiement.</a> / <a href="{{route('payment.start')}}" lang="en-CA">Make another payment.</a></p>
     </section>
     {!! NoCaptcha::renderJs('fr-CA') !!}
 </x-layout>
