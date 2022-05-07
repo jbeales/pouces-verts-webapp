123a9a4dba46e0c9e6c12d63c10c700be9b5cc41
John Beales <john@johnbeales.com>
2022-05-06 23:50:31 -0400

    Try not to overflow the screen.

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -54,14 +54,21 @@ class PaymentController extends Controller
         Stripe::setApiKey(config('services.stripe.secret_key'));
         $session = Session::create($checkout_data);
 
+        session(['stripe_session' => $session['id']]);
+
         return redirect($session->url);
 
     }
 
 
     public function success(string $checkout_session_id, Request $request) {
-        Stripe::setApiKey(config('services.stripe.secret_key'));
-        $session = Session::retrieve($checkout_session_id);
+        if( session('stripe_session') === $checkout_session_id ) {
+            Stripe::setApiKey(config('services.stripe.secret_key'));
+            $session = Session::retrieve($checkout_session_id);
+        } else {
+            $session = null;
+        }
+
         return view('payment.success', ['stripe_session' => $session]);
     }
 
